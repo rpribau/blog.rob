b4327ffeff19de0bcd814cdd212d5bab11968859
Rob <106797059+rpribau@users.noreply.github.com>
2023-05-22 11:24:42 -0600

    Box and some text created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
 const Page = () => {
     return (
-        <div>
-            <p>Hello</p>
-        </div>
-    )
+    <Container>
+        <Box borderRadius="lg" bg ="azure" p={3} mb={5} align="center">
+            Hello, I&apos;m a student at Tecnologico de Monterrey and I&apos;m currently studing B.S in Data Science and Mathematics.
+        </Box>
+
+        <Box display = {{md: 'flex'}}>
+        <Box flexGrow={1}>
+        <Heading as ="h2" variant="page-title">
+            Roberto Priego Bautista
+        </Heading>
+
+      <p>@rpribau ( Developer / Coding / FIRST Alumni)</p>
+        </Box>
+    </Box>
+
+    </Container>
+   )
 }
 
 export default Page
