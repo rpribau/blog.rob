f595ef9915248f445c6405d55505d9122d808d79
Rob <106797059+rpribau@users.noreply.github.com>
2023-05-30 11:16:06 -0600

    Another fix even if About is broke

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,26 @@
-// import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
-// import Section from "../components/section";
+import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+import Section from "../components/section";
 
-// const About = () => {
-//     return(
-//         <Container>
-//             <Heading as "h3" fontSize={20} mb={4}>
-//                 About Me
-//             </Heading>
+const About = () => {
+    return(
+        <Container>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    About Me
+                    
+                </Heading>
+                <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                    <Heading as="h4" variant="section-title">
+                        Hi, I'm Roberto Priego Bautista
+                    </Heading>
+                    <Heading as="h4" variant="section-title">
+                        I'm a student at Tecnologico de Monterrey based in Mexico!
+                    </Heading>
+                </SimpleGrid>
+            </Section>
             
-//         </Container>
-//     )
-// }
+        </Container>
+    )
+}
 
-// export default About;
+export default About;
