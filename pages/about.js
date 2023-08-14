b6567be9d1dca634c14fb2ce975371e454ce2b5b
Rob <106797059+rpribau@users.noreply.github.com>
2023-08-14 13:43:56 -0600

    HackBanorte almost done.

--- pages/about.js
+++ pages/about.js
@@ -1,59 +1,64 @@
-import React, { useState, useRef } from 'react'; // Añadir useRef
-import { Box, 
-         Button, 
-         Modal, 
-         ModalOverlay, 
-         ModalContent, 
-         ModalHeader, 
-         ModalCloseButton, 
-         ModalBody, 
-         ModalFooter, 
-         useDisclosure, 
-        } 
-from '@chakra-ui/react';
-
+import React, { useState, useRef } from 'react';
+import { Box, Button, useDisclosure, Image } from '@chakra-ui/react';
 import ImageBox from '../components/imagebox';
+import ReusableModal from '../components/reusable-modal';
 
 function BlogPage() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const finalRef = useRef(); // Crear la referencia aquí
+  const { isOpen: isOpenModal1, onOpen: onOpenModal1, onClose: onCloseModal1 } = useDisclosure();
+  const { isOpen: isOpenModal2, onOpen: onOpenModal2, onClose: onCloseModal2 } = useDisclosure();
+  
+  const finalRefModal1 = useRef();
+  const finalRefModal2 = useRef();
 
-  const handleImageClick = () => {
-    onOpen();
+  const handleImageClickModal1 = () => {
+    onOpenModal1();
+  };
+
+  const handleImageClickModal2 = () => {
+    onOpenModal2();
   };
 
   return (
     <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
-      <ImageBox
-        imageUrl="https://i.imgur.com/yTLHHab.png"
-        onClick={handleImageClick}
-      />
+      <ImageBox imageUrl="https://i.imgur.com/yTLHHab.png" onClick={handleImageClickModal1} />
+      <Box mb={4} />
+      <ImageBox imageUrl="https://i.imgur.com/QQYlhb8.png" onClick={handleImageClickModal2} />
 
-      <Box mb={4} /> {/* Espacio de separación hacia abajo */}
+      <ReusableModal isOpen={isOpenModal1} onClose={onCloseModal1} finalRef={finalRefModal1} title="Hackathon Banorte - 1st place in category and event">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="596px"
+          width="100%"
+          p="4" // Espaciado interno
+        >
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7072265191186386944"
+            height="596"
+            width="454"
+            allowFullScreen
+            title="Publicación integrada"
+          ></iframe>
+          <Image src="https://i.imgur.com/JNub3us.png" alt="Additional Image" maxH="596px" ml="4" />
+        
+        </Box>
+        
+        <Box mt={8} alignContent='center'>
 
-      <ImageBox
-        imageUrl="https://i.imgur.com/QQYlhb8.png"
-        onClick={handleImageClick}
-      />
-
-    <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
-    <ModalOverlay />
-    <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-        This is some modal content.
-        </ModalBody>
-
-        <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-            Close
-        </Button>
-        <Button variant='ghost'>Secondary Action</Button>
-        </ModalFooter>
-    </ModalContent>
-    </Modal>
-
+          Probably the most demanding project I've ever had, the Banorte Hackathon was one of the best experiences I had sharing with several of my best friends, 
+          friends who were and are part of the VOLTEC ROBOTICS 6647 team. In this challenge there was a focus on improving the experience of the user using the Banorte 
+          portal using artificial intelligence. Tools such as Pytorch were used to generate a script that would allow knowing where the user's mouse position is to 
+          determine the time it took to reach a portal service. As well as based on artificial intelligence, which will generate a new version of the Banorte portal 
+          and determine how long it takes. If the AI made the process faster than the human, this user will receive an update from his portal so that he can make that 
+          process faster. For the generation of web pages, a prototype of a new Banorte portal was created using Remix, a JavaScript framework and with additional tools 
+          such as TailWind CSS and DaisyUI to generate friendlier and easier-to-use pages.
+        </Box>
+      </ReusableModal>
+      
+      <ReusableModal isOpen={isOpenModal2} onClose={onCloseModal2} finalRef={finalRefModal2} title="Modal 2 Title">
+        This is some content for Modal 2.
+      </ReusableModal>
     </Box>
   );
 }
