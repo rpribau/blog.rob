358039e2d52f500a3717ee9ab983029d064d5018
Rob <106797059+rpribau@users.noreply.github.com>
2023-08-21 14:18:55 -0600

    RoboRAVE almost complete.

--- pages/about.js
+++ pages/about.js
@@ -28,6 +28,7 @@ function BlogPage() {
   };
 
   const githubLink = "https://github.com/dvddvd300/Banorte-2023";
+  const githubLink2 = "https://github.com/rpribau/Roborave-Trix";
   const test = "https://banorte-2023.pages.dev/?lng=es"
 
   const handleGitHubClick = () => {
@@ -36,6 +37,10 @@ function BlogPage() {
 
   const handleTestClick = () => {
     window.open(test, '_blank');
+  };
+
+  const handleGitHubClick2 = () => {
+    window.open(githubLink2, '_blank');
   };
 
   return (
@@ -105,8 +110,33 @@ function BlogPage() {
         </HStack>
       </ReusableModal>
       
-      <ReusableModal isOpen={isOpenModal2} onClose={onCloseModal2} finalRef={finalRefModal2} title="Modal 2 Title">
-        This is some content for Modal 2.
+      <ReusableModal isOpen={isOpenModal2} onClose={onCloseModal2} finalRef={finalRefModal2} title="RoboRAVE Monterrey Invitational">
+      This is my last project to date in which I participate in the FireFighting category. It consisted of building a 100% autonomous robot 
+      that could put out candles that are at different heights and blocked by walls. Just like the sails were surrounded by black tape that 
+      marked an area for the sails that could be used by sensors to align the robot. I participated with my team called Robotrix, made up mainly 
+      of Mechatronic and Robotic Engineers. While they were mainly in charge of the function of motors and sensors, I personally was in charge of 
+      implementing a pretrained model of object detection with YOLOv5 and using an NVIDIA Jetson to process video and that this would send signals 
+      to the Arduino to move motors. The way in which I programmed it was using Python libraries such as Pytorch, OpenCV, Numpy, Ultralytics (in the beta phase) 
+      and Pyserial (library in charge of communicating with the Arduino).
+
+      <Box mt={8} alignContent='center'>
+
+      <iframe width="360" height="515" src="https://www.youtube.com/embed/qeLvfSFmh5M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+      </Box>
+
+      <HStack spacing={4} mt={4}>
+        <Button
+            leftIcon={<AiFillGithub />}
+            colorScheme="purple"
+            onClick={handleGitHubClick2} 
+        >
+            GitHub Repo
+          </Button>
+
+
+        </HStack>
+
       </ReusableModal>
 
       <ReusableModal isOpen={isOpenModal3} onClose={onCloseModal3} finalRef={finalRefModal3} title="Modal 3 Title">
