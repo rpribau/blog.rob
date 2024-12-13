ac1f6b024d84b529c59a4ca5ec7623d915913aa3
rpribau <a01285539@tec.mx>
2024-12-12 21:02:02 -0600

    Add 'Blog' link to navbar with a 'NEW' badge

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,26 +12,41 @@ export function Navbar() {
     <header className="fixed top-0 left-0 right-0 bg-background z-10 border-b border-border">
       <div className="container mx-auto px-4 max-w-5xl">
         <div className="flex h-16 items-center justify-between">
-          <MonitorSmartphone className="h-6 w-6 text-primary" />
-          <nav className="flex items-center gap-6">
-            <Link 
-              href="/about"
-              className="text-sm font-medium transition-colors hover:text-primary"
-            >
-              About
-            </Link>
-            <Link 
-              href="/works"
-              className="text-sm font-medium transition-colors hover:text-primary"
-            >
-              Works
-            </Link>
-            <Link 
-              href="/cv"
-              className="text-sm font-medium transition-colors hover:text-primary"
-            >
-              CV
-            </Link>
+          <div className="flex items-center gap-6">
+            <MonitorSmartphone className="h-6 w-6 text-primary" />
+            <nav className="flex items-center gap-6">
+              <Link 
+                href="/about"
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                About
+              </Link>
+              <Link 
+                href="/works"
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                Works
+              </Link>
+              <Link 
+                href="/cv"
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                CV
+              </Link>
+              <div className="relative">
+                <Link 
+                  href="/blog"
+                  className="text-sm font-medium transition-colors hover:text-primary"
+                >
+                  Blog
+                  <span className="absolute -top-2.5 -right-6.5 bg-red-600 text-white text-[9px] px-1 py-0.75 rounded-full">
+                    NEW
+                  </span>
+                </Link>
+              </div>
+            </nav>
+          </div>
+          <div className="flex items-center gap-4">
             <Link 
               href="https://github.com"
               target="_blank"
@@ -56,7 +71,7 @@ export function Navbar() {
               <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
             </Button>
-          </nav>
+          </div>
         </div>
       </div>
     </header>
